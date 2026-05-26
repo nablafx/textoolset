3b9d6ad7ea9ebf4da4484be8d0ba1e1988a1c0d1
Jerry Zhang <fu.zhang1@outlook.com>
2026-05-26 12:53:26 +0800

    changed build opinion

--- build_macos.sh
+++ build_macos.sh
@@ -3,8 +3,9 @@
 # Note: macOS uses the colon ':' as the path separator for --add-data
 
 pyinstaller --noconsole --clean \
-  --name "TeXToolset" \
+  --name "TexToolkit" \
   --distpath "build" \
+  --workpath "build" \
   --add-data "templates:templates" \
   --hidden-import "jinja2.ext" \
   main_desktop.py
--- build_windows.bat
+++ build_windows.bat
@@ -3,8 +3,9 @@ REM PyInstaller build command for Windows
 REM Note: Windows uses the semicolon ';' as the path separator for --add-data
 
 pyinstaller --noconsole --clean ^
-  --name "TeXToolset" ^
+  --name "TexToolkit" ^
   --distpath "build" ^
+  --workpath "build" ^
   --add-data "templates;templates" ^
   --hidden-import "jinja2.ext" ^
   main_desktop.py
